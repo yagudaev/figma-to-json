2a2dc77887e41a4ff946f98a55fac47cb0c03825
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-09-16 13:03:16 -0700

    Initial commit bootstrapping plugin

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  trailingComma: "none",
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  singleQuote: false,
+  printWidth: 100,
+  semi: false
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { loadFontsAsync, once, showUI } from "@create-figma-plugin/utilities"
+import { InsertCodeHandler } from "./types"
+
+export default function () {
+  once<InsertCodeHandler>("INSERT_CODE", async function (code: string) {
+    const text = figma.createText()
+    await loadFontsAsync([text])
+    text.characters = code
+    figma.currentPage.selection = [text]
+    figma.viewport.scrollAndZoomIntoView([text])
+    figma.closePlugin()
+  })
+  showUI({ height: 240, width: 320 })
+}
