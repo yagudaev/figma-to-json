ee2f4ee4b350bbd5141d41d3c01d76de0dd09186
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-09-20 13:19:06 -0700

    Setup a mono repo for the plugin

--- bin/fig2json.ts
+++
@@ -1,8 +0,0 @@
-import { compileSchema } from "kiwi-schema"
-import { readFileSync, writeFileSync } from "fs"
-
-const schema = compileSchema(readFileSync("bin/schema.kiwi", "utf8") as any)
-const file = readFileSync("bin/test.fig", "binary")
-const decoded = schema.decodeDocument(file)
-
-console.log("decoded", decoded)
