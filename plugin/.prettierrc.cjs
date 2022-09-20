d0b71692e1692b56cf223b8de9a2628000bfef94
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-09-20 13:36:10 -0700

    Get mantine setup

--- plugin/.prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: "none",
-  tabWidth: 2,
-  jsxSingleQuote: true,
-  singleQuote: false,
-  printWidth: 100,
-  semi: false
-}
--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app"
+import Head from "next/head"
+import { MantineProvider } from "@mantine/core"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light"
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
 }
-
-export default MyApp
